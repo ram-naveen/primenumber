f8bc59db948f1a62f1b89154c93124dc63e9d6fa
naveen1100 <naveen@golangbot.com>
2023-09-09 12:47:23 +0100

    added more integration test cases

--- src/test/java/com/natwest/primenumber/PrimeNumberIntegrationTest.java
+++ src/test/java/com/natwest/primenumber/PrimeNumberIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.natwest.primenumber;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,16 +21,22 @@ public class PrimeNumberIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void testGeneratePrimesEndpoint() throws JSONException {
-        String url = "http://localhost:" + port + "/primes/10";
+    @ParameterizedTest
+    @CsvSource(value={
+    		"0#{\"Initial\":0,\"Primes\":[]}",           		// Input: http://localhost:8080/primes/0, Expected: {"Initial":0,"Primes":[]}
+    		"1#{\"Initial\":1,\"Primes\":[]}",           		// Input: http://localhost:8080/primes/1, Expected: {"Initial":1,"Primes":[]}
+            "10#{\"Initial\":10,\"Primes\":[2,3,5,7]}",  		// Input: http://localhost:8080/primes/10, Expected: {"Initial":10,"Primes":[2,3,5,7]}
+            "15#{\"Initial\":15,\"Primes\":[2,3,5,7,11,13]}",  	// Input: http://localhost:8080/primes/10, Expected: {"Initial":10,"Primes":[2,3,5,7]}
+    }, delimiter = '#')
+    public void testGeneratePrimesEndpoint(int input, String expectedResponseBody) throws JSONException {
+        String url = "http://localhost:" + port + "/primes/" + input;
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         assertEquals(200, responseEntity.getStatusCode().value());
         
         String responseBody = responseEntity.getBody();
         assertNotNull(responseBody);
         
-        String expectedResponseBody = "{\"Initial\":10,\"Primes\":[2,3,5,7]}";
+       // String expectedResponseBody = "{\"Initial\":10,\"Primes\":[2,3,5,7]}";
 		JSONAssert.assertEquals(expectedResponseBody, responseBody, true);
     }
 }
