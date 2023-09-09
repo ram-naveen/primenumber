3351c29953340934c30999118a2a1eb889d4dd06
naveen1100 <naveen@golangbot.com>
2023-09-09 13:44:50 +0100

    organizing imports

--- src/main/java/com/natwest/primenumber/service/PrimeNumberService.java
+++ src/main/java/com/natwest/primenumber/service/PrimeNumberService.java
@@ -2,7 +2,6 @@ package com.natwest.primenumber.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
